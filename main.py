9baa4db1a21344e0ca67662b7034491392c2a044
christo357 <christomathew2001@gmail.com>
2024-11-18 10:56:46 -0500

    final main(without gui)

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import random
 import re
 from time import sleep  # To add delay between steps
 from cell import Cell
+from multiprocessing import Process
+
 # from fire import Fire
 
 
 
 from logger import Logger
 from ship import Ship
-from ui import ShipInterface
+# from gui import GridGUI 
 import bot1 as bot1s
 import bot2 as bot2s
 import bot1_m as bot1m
-import bot1_m_copy as bot2m
+import bot2_m as bot2m
 
 # Constants
 SIZE = 30
 ALPHA = .1
 CELL_SIZE = 15
+RANDOM_SEED = random.randint(0, 10000)
 GRID_WIDTH = SIZE
 GRID_HEIGHT = SIZE
 WINDOW_WIDTH = GRID_WIDTH * CELL_SIZE
@@ -32,15 +35,17 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 FIRE_COLOR = (255, 69, 0)
-        
-pygame.init()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Space Rat Simulation")
 
-RANDOM_SEED = random.randint(0, 10000)
+resultFolder = "results"
+        
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
 my_ship = Ship(SIZE, RANDOM_SEED)
 my_ship.createShip()
-interface1 = ShipInterface( SIZE, CELL_SIZE, 1, my_ship.getOpenCells())
+# interface1 = ShipInterface( SIZE, CELL_SIZE, 1, my_ship.getOpenCells())
 r_b, c_b = random.choice(my_ship.getOpenCells())
 print(f"initial bot loc : {r_b, c_b}")
 my_ship.start_botloc =  (r_b, c_b)
@@ -50,19 +55,40 @@ rat_init = my_ship.getRatloc()
 
 ### Bot 1 with stationary rat
 # my_ship.setRatloc(rat_init)
-my_bot1s = bot1s.Bot(my_ship, r_b, c_b,alpha=ALPHA, interface=interface1, seed=RANDOM_SEED)
+b1_resultPath = resultFolder+"/b1"
+create_folder_if_not_exists(b1_resultPath)
+b1_path = f"{b1_resultPath}/{SIZE}_{ALPHA}"
+my_bot1s = bot1s.Bot(my_ship, r_b, c_b,alpha=ALPHA, seed=RANDOM_SEED, resultPath = b1_path)
 getPos = my_bot1s.findPosition()
 bot1s_len = len(my_ship.ratPositions)
 
-if getPos:
+if getPos is not (0,0):
     print("FINDING RAT")
     steps1s = my_bot1s.findRat()
     bot1s_rat = my_ship.getRatPositions()
     
+### Bot 1 with moving rat
+my_ship.setRatloc(rat_init)
+b2_resultPath = resultFolder+"/b2"
+create_folder_if_not_exists(b2_resultPath)
+b2_path = f"{b2_resultPath}/{SIZE}_{ALPHA}"
+my_bot1m = bot1m.Bot(my_ship, r_b, c_b,alpha=ALPHA, seed=RANDOM_SEED, resultPath = b2_path)
+getPos = my_bot1m.findPosition()
+bot1m_len = len(my_ship.ratPositions)
 
+if getPos:
+    print("FINDING RAT")
+    steps1m = my_bot1m.findRat()
+    bot1m_ratpos = my_ship.getRatPositions()
+    bot1m_rat = my_ship.getRatloc()
+    
+    
 ## Bot  2 with stationary rat
 my_ship.setRatloc(rat_init)
-my_bot2s = bot2s.Bot(my_ship, r_b, c_b,alpha=ALPHA, interface=interface1, seed=RANDOM_SEED)
+b3_resultPath = resultFolder+"/b3"
+create_folder_if_not_exists(b3_resultPath)
+b3_path = f"{b3_resultPath}/{SIZE}_{ALPHA}"
+my_bot2s = bot2s.Bot(my_ship, r_b, c_b,alpha=ALPHA, seed=RANDOM_SEED, resultPath = b3_path)
 getPos = my_bot2s.findPosition()
 bot2s_len = len(my_ship.ratPositions)
 
@@ -74,21 +100,15 @@ if getPos:
 
 
 
-### Bot 1 with moving rat
-my_ship.setRatloc(rat_init)
-my_bot1m = bot1m.Bot(my_ship, r_b, c_b,alpha=ALPHA, interface=interface1, seed=RANDOM_SEED)
-getPos = my_bot1m.findPosition()
-bot1m_len = len(my_ship.ratPositions)
 
-if getPos:
-    print("FINDING RAT")
-    steps1m = my_bot1m.findRat()
-    bot1m_ratpos = my_ship.getRatPositions()
-    bot1m_rat = my_ship.getRatloc()
 
 ### Bot  2 with moving rat
 my_ship.setRatloc(rat_init)
-my_bot2m = bot2m.Bot(my_ship, r_b, c_b,alpha=ALPHA, interface=interface1, seed=RANDOM_SEED)
+my_ship.setRatloc(rat_init)
+b4_resultPath = resultFolder+"/b4"
+create_folder_if_not_exists(b4_resultPath)
+b4_path = f"{b4_resultPath}/{SIZE}_{ALPHA}"
+my_bot2m = bot2m.Bot(my_ship, r_b, c_b,alpha=ALPHA,  seed=RANDOM_SEED, resultPath = b4_path)
 getPos = my_bot2m.findPosition()
 bot2m_len = len(my_ship.ratPositions)
 
@@ -99,59 +119,33 @@ if getPos:
     bot2m_rat = my_ship.getRatloc()
     
     
-    
-print(f"Bot 1 rat len: {bot1s_len}")
+print("Baseline bot (stationary  rat)")
+print("------------------------------")
+print(f"Baseline bot (stationary  rat) len: {bot1s_len}")
 # print(f"Bot 1 rat: {bot1s_rat}")
-print(f"Total steps: bot1: {steps1s}, rat: {bot1s_rat}")
+print(f"Total steps:  {steps1s}, ")
+print(f"found rat at: {bot1s_rat}")
 
-
-print(f"Bot 1 rat len: {bot1m_len}")
+print()
+print("Baseline bot (moving  rat)")
+print("------------------------------")
+print(f"Baseline bot (moving  rat) len: {bot1m_len}")
 # print(f"Bot 1 rat: {bot1m_rat}")
-print(f"Total steps: bot1: {steps1m}, rat: {bot2s_rat}")
+print(f"Total steps:  {steps1m},")
+print(f"rat: {bot1m_rat}")
 
-print(f"Bot 2 rat len: {bot2s_len}")
+print()
+print("Improved bot (moving  rat)")
+print("------------------------------")
+print(f"Improved bot (moving  rat) len: {bot2s_len}")
 # print(f"Bot 2 rat: {bot2s_rat}")
-print(f"Total steps: bot2: {steps2s}, rat: {bot1m_rat}")
+print(f"Total steps: bot2s: {steps2s}, rat: {bot2s_rat}")
 
-print(f"Bot 2 rat len: {bot2m_len}")
+print()
+print("Improved bot (moving  rat)")
+print("------------------------------")
+print(f"Improved bot (moving  rat)len: {bot2m_len}")
 # print(f"Bot 2 rat: {bot2m_rat}")
-print(f"Total steps: bot2: {steps2m}, rat: {bot2m_rat}")
+print(f"Total steps: bot2m: {steps2m}, rat: {bot2m_rat}")
 
 
-
-
-
-
-
-
-# def initializePygame():
-#     pygame.init()
-#     # Create pygame window
-#     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-#     pygame.display.set_caption("Ship Fire Simulation")
-    
-# # Function to log metadata (grid size, bot, and switch location) to result.txt
-# def log_metadata(ship, resultPath):
-    
-#     with open(resultPath, "w") as file:
-#         file.write(f"Grid Size: {GRID_WIDTH}x{GRID_HEIGHT}\n")
-#         # file.write(f"Flammability: {ship.get_q()}\n")
-#         # file.write(f'Bot : {bot.get_Id()}\n')
-#         bot_row, bot_col = ship.getStartBotLoc()
-#         rat_row, rat_col = ship.getRatloc()
-#         file.write(f"Initial Bot Location: ({bot_row}, {bot_col})\n")
-#         file.write(f"Switch Location: ({rat_row}, {rat_col})\n")
-#         file.write("-" * 40 + "\n")
-
-# # Function to log the grid state to result.txt at each timestep
-# def log_grid_state(ship, timestep, bot_count, resultPath):
-#     with open(resultPath, "a") as file:
-#         file.write(f"Timestep: {timestep}\n")
-#         for row in range(GRID_HEIGHT):
-#             for col in range(GRID_WIDTH):
-#                 file.write(ship.get_cellval(row, col))
-#             file.write("\n")
-        
-#         bot_row, bot_col = ship.getBotLoc(i)
-#         file.write(f"Bot {i+1}: ({bot_row}, {bot_col})\n")
-#         file.write("-" * 40 + "\n")
